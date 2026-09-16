01b494b76af6a7c20c47c371985b340e6ad506c6
shubh <shubh@poly.local>
2026-09-16 18:41:41 +0530

    chore: ignore tests and local-only dirs, untrack tests folder

--- tests/test_polymarket_rate_limit.py
+++
@@ -1,8 +0,0 @@
-from src.utils.polymarket_rate_limit import DEFAULT_LIMITS
-
-
-def test_data_api_limit_configuration_is_explicit_and_nonzero():
-    assert set(DEFAULT_LIMITS) == {"positions", "closed-positions", "trades", "activity"}
-    for rate, capacity in DEFAULT_LIMITS.values():
-        assert rate > 0
-        assert capacity >= 1
